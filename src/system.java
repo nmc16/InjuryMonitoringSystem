94e83f7f6ee5a9f9f83ea4fd4e4bbcf54cd81b4d
Abbas Zahed <dx_7171@hotmail.com>
2015-11-13 13:14:11 -0500

    commited response

---
+++ src/system.java
@@ -0,0 +1,21 @@
+import java.lang.Object;
+import java.util.concurrent.Callable;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+
+public class system {
+	public static void main(String[] args) throws InterruptedException {
+		final GpioController gpio = GpioFactory.getInstance();
+		final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+		
+	}
+}
