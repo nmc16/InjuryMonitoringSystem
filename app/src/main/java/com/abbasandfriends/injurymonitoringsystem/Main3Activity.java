9b2937b808675b7a73e6caea2ec8499756ceb4fa
Luke Sanderson <luke@Lukes-MacBook-Air.local>
2015-11-19 22:37:28 -0500

    Created reference to array in activity1, in activity2

--- app/src/main/java/com/abbasandfriends/injurymonitoringsystem/Main3Activity.java
+++ app/src/main/java/com/abbasandfriends/injurymonitoringsystem/Main3Activity.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
-
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -23,7 +22,7 @@ public class Main3Activity extends android.app.Activity {
 
         BarData data = new BarData(getXAxisValues(), getDataSet());
         chart.setData(data);
-        chart.setDescription("My Chart");
+        chart.setDescription("Impact Monitoring");
         chart.animateXY(2000, 2000);
         chart.invalidate();
     }
@@ -32,52 +31,48 @@ public class Main3Activity extends android.app.Activity {
         ArrayList<BarDataSet> dataSets = null;
 
         ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        BarEntry v1e1 = new BarEntry(30.000f, 0); // Jan
         valueSet1.add(v1e1);
         BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
         valueSet1.add(v1e2);
         BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
         valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        BarEntry v1e4 = new BarEntry(130.000f, 3); // Apr
         valueSet1.add(v1e4);
         BarEntry v1e5 = new BarEntry(90.000f, 4); // May
         valueSet1.add(v1e5);
         BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
         valueSet1.add(v1e6);
+        BarEntry v1e7 = new BarEntry(30.000f, 6); // Jan
+        valueSet1.add(v1e7);
+        BarEntry v1e8 = new BarEntry(40.000f, 7); // Feb
+        valueSet1.add(v1e8);
+        BarEntry v1e9 = new BarEntry(60.000f, 8); // Mar
+        valueSet1.add(v1e9);
 
-        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-        valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-        valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-        valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-        valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-        valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-        valueSet2.add(v2e6);
 
-        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
-        barDataSet1.setColor(Color.rgb(0, 155, 0));
-        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Change in Acceleration");
+        barDataSet1.setColor(Color.rgb(155, 0, 0));
+        //BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
+        //barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
 
         dataSets = new ArrayList<>();
         dataSets.add(barDataSet1);
-        dataSets.add(barDataSet2);
+        //dataSets.add(barDataSet2);
         return dataSets;
     }
 
     private ArrayList<String> getXAxisValues() {
         ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
+        xAxis.add("Time1");
+        xAxis.add("Time2");
+        xAxis.add("Time3");
+        xAxis.add("Time4");
+        xAxis.add("Time5");
+        xAxis.add("Time6");
+        xAxis.add("Time7");
+        xAxis.add("Time8");
+        xAxis.add("Time9");
         return xAxis;
     }
 }
