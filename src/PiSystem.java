3c5b2515eedf494d51c39fc35b305319f6c32e4b
Abbas <dx_7171@hotmail.com>
2015-11-27 14:07:24 -0500

    Commited code

--- src/PiSystem.java
+++ src/PiSystem.java
@@ -1,73 +1,40 @@
+// imports for java
 import java.io.IOException;
 import com.pi4j.component.switches.SwitchListener;
 import com.pi4j.component.switches.SwitchState;
 import com.pi4j.component.switches.SwitchStateChangeEvent;
 import com.pi4j.device.piface.PiFace;
 import com.pi4j.device.piface.PiFaceLed;
-import com.pi4j.device.piface.PiFaceRelay;
 import com.pi4j.device.piface.PiFaceSwitch;
 import com.pi4j.device.piface.impl.PiFaceDevice;
 import com.pi4j.wiringpi.Spi;
 
 public class PiSystem {
-	static int cylonSpeed = 100;
-	
-	public static void main(String args[]) throws InterruptedException, IOException {
-		final PiFace piface = new PiFaceDevice(PiFace.DEFAULT_ADDRESS, Spi.CHANNEL_0);
-		
-		piface.getSwitch(PiFaceSwitch.S1).addListener(new SwitchListener() {
-			@Override
-			public void onStateChange(SwitchStateChangeEvent event) {
-				if(event.getNewState() == SwitchState.ON) {
-					piface.getRelay(PiFaceRelay.K0).close();
-				}
-				else {
-					piface.getRelay(PiFaceRelay.K0).open();
-				}
-			}
-		});
-		
-		piface.getSwitch(PiFaceSwitch.S2).addListener(new SwitchListener() {
-			@Override
-			public void onStateChange(SwitchStateChangeEvent event) {
-				if(event.getNewState() == SwitchState.ON) {
-					piface.getRelay(PiFaceRelay.K1).toggle();
-				}
-				else {
-				}
-			}
-		});
-		
-		piface.getSwitch(PiFaceSwitch.S3).addListener(new SwitchListener() {
-			@Override
-			public void onStateChange(SwitchStateChangeEvent event) {
-				if(event.getNewState() == SwitchState.ON) {
-					piface.getLed(PiFaceLed.LED2).blink(125);
-				}
-				else {
-					piface.getLed(PiFaceLed.LED2).blink(0);
-					piface.getLed(PiFaceLed.LED2).off();
-				}
-			}
-		});
-		
-		piface.getSwitch(PiFaceSwitch.S4).addListener(new SwitchListener() {
-			@Override
-			public void onStateChange(SwitchStateChangeEvent event) {
-				if(event.getNewState() == SwitchState.ON) {
-					cylonSpeed = 30;
-				}
-				else {
-					cylonSpeed = 100;
-				}
-			}
-		});
-		
-		while(true) {
-			for (int index = PiFaceLed.LED3.getIndex(); index >= PiFaceLed.LED3.getIndex(); index--) {
-				piface.getLed(index).pulse(cylonSpeed);
-				Thread.sleep(cylonSpeed);
-			}
-		}
-	}
+	private static PiFace piFace;
+    // variables used to delay action for a set period of time
+    private static final int onTime = 125;
+    
+    public PiSystem() throws IOException {
+    	piFace = new PiFaceDevice(PiFace.DEFAULT_ADDRESS, Spi.CHANNEL_0);
+    }
+    //method for third switch and its functionality
+    public void setUpButtonHandler() {
+        // creates listener
+    	piFace.getSwitch(PiFaceSwitch.S1).addListener(new SwitchListener() {
+            @Override
+            // checks for if the switch is on and if it is turns on light for set amount of time
+            public void onStateChange(SwitchStateChangeEvent event) {
+                if(event.getNewState() == SwitchState.ON) {
+                	piFace.getLed(PiFaceLed.LED2).blink(onTime);
+                } else {
+                	piFace.getLed(PiFaceLed.LED2).off();
+                }
+            }
+        });
+    }
+    // main method to create PiFace controller and use methods
+    public static void main(String args[]) throws InterruptedException, IOException {
+    	PiSystem emergency = new PiSystem();
+    	emergency.setUpButtonHandler();
+    }
 }
