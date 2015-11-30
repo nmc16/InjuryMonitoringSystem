562352d173642fcdc7192992abe8522670b6b1d5
Abbas <dx_7171@hotmail.com>
2015-11-30 15:47:32 +0000

    commited code

--- src/controller/PiSystem.java
+++ src/controller/PiSystem.java
@@ -11,13 +11,14 @@ import com.pi4j.device.piface.impl.PiFaceDevice;
 import com.pi4j.wiringpi.Spi;
 
 public class PiSystem {
+	// creates global variables
 	private static PiFace piFace;
 	private static final int onTime =125;
-
+	// constructor to intialize variable
 	public PiSystem() throws IOException {
 		piFace = new PiFaceDevice(PiFace.DEFAULT_ADDRESS, Spi.CHANNEL_0);
 	}
-
+	// button handler to initialize the lights to start blinking and when button is pressed turn off
 	public void setUpButtonHandler() {
 		piFace.getLed(PiFaceLed.LED0).blink(500);
 		piFace.getLed(PiFaceLed.LED1).blink(500);
@@ -27,14 +28,15 @@ public class PiSystem {
 		piFace.getLed(PiFaceLed.LED5).blink(500);
 		piFace.getLed(PiFaceLed.LED6).blink(500);
 		piFace.getLed(PiFaceLed.LED7).blink(500);
-
+		// button listener to add the buttons and imply the switch is clicked
 		piFace.getSwitch(PiFaceSwitch.S1).addListener(new SwitchListener() {
 			@Override
+			// the lights are changed to off with blink and off
 			public void onStateChange(SwitchStateChangeEvent event) {
 				if(event.getNewState() == SwitchState.ON) {
 					piFace.getLed(PiFaceLed.LED0).blink(0);
 					piFace.getLed(PiFaceLed.LED0).off();
-					piFace.getRelay(PiFaceRelay.K0).open();
+					//piFace.getRelay(PiFaceRelay.K0).open();
 					piFace.getLed(PiFaceLed.LED1).blink(0);
 					piFace.getLed(PiFaceLed.LED1).off();
 					piFace.getLed(PiFaceLed.LED2).blink(0);
@@ -53,7 +55,7 @@ public class PiSystem {
 			}
 		});	
 	}
-
+	// main method to run the code
 	public static void main(String args[]) throws InterruptedException, IOException {
 		PiSystem emergency = new PiSystem();
 		emergency.setUpButtonHandler();
