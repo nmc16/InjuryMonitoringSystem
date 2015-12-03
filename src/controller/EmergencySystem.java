b25fe431ce4093255e764c9363c24d41177db89f
Abbas Zahed <dx_7171@hotmail.com>
2015-12-02 19:57:25 -0500

    Updated

--- src/controller/EmergencySystem.java
+++ src/controller/EmergencySystem.java
@@ -31,9 +31,10 @@ import exception.CommunicationException;
 public class EmergencySystem implements Consumer {
 	// creates global variables
 	private PiFace piFace;
-	
-	
+	private final int LED_ON = 500;
+	private final int LED_OFF = 0;
 	private final int BUFFER_SIZE = 65536;
+	private final int MAX_CONNECTION = 50;
 	private Gson gson = new GsonBuilder().registerTypeAdapter(Sendable.class, new SendableDeserializer()).create();
 	private ServerSocket server;
 	private String hostIP;
@@ -45,29 +46,29 @@ public class EmergencySystem implements Consumer {
 	public String getHostIP(){
 		return hostIP;	
 	}
+	
 	public int getHostPort(){
 		return hostPort;	
 	}
+	
 	public Socket getHostSocket(){
 		return hostSocket;	
 	}
 	
-	
 	// setters 
-	
 	public void setHostIP(String IP){
 		this.hostIP= IP;
 	}
 	
-	
 	public void setHostPort(int clientPort){
 		this.hostPort=clientPort;
 	}
+	
 	public void setHostSocket(Socket clientSocket){	
 		this.hostSocket=clientSocket;
 	}
 	
-	// constructor to intialize variable
+	// constructor to initialize variable and create new object of the class
 	public EmergencySystem() throws IOException {
 		piFace = new PiFaceDevice(PiFace.DEFAULT_ADDRESS, Spi.CHANNEL_0);
 		
@@ -85,14 +86,12 @@ public class EmergencySystem implements Consumer {
 		
 		// Start the server and create the input socket
 		try {
-			server = new ServerSocket(hostPort, 50, ip);
+			server = new ServerSocket(hostPort, MAX_CONNECTION, ip);
 
 		} catch (IOException e) {
 			throw new CommunicationException("Could not open server socket or input stream", e);
 		}
 	}
-
-
 
 	/**
 	 * @see Consumer#acceptClient()
@@ -166,99 +165,93 @@ public class EmergencySystem implements Consumer {
             throw new CommunicationException("Could not close server socket", e);
         }
 	}
-
-	// button handler to initialize the lights to start blinking and when button is pressed turn off
+	
+	/**
+     * button handler to initialize the lights to start blinking
+     * 
+     * When button is pressed turn off
+     */
 	public void setUpButtonHandler() {
-		piFace.getLed(PiFaceLed.LED0).blink(500);
-		piFace.getLed(PiFaceLed.LED1).blink(500);
-		piFace.getLed(PiFaceLed.LED2).blink(500);
-		piFace.getLed(PiFaceLed.LED3).blink(500);
-		piFace.getLed(PiFaceLed.LED4).blink(500);
-		piFace.getLed(PiFaceLed.LED5).blink(500);
-		piFace.getLed(PiFaceLed.LED6).blink(500);
-		piFace.getLed(PiFaceLed.LED7).blink(500);
+		piFace.getLed(PiFaceLed.LED0).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED1).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED2).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED3).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED4).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED5).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED6).blink(LED_ON);
+		piFace.getLed(PiFaceLed.LED7).blink(LED_ON);
 		// button listener to add the buttons and imply the switch is clicked
 		piFace.getSwitch(PiFaceSwitch.S1).addListener(new SwitchListener() {
 			@Override
 			// the lights are changed to off with blink and off
 			public void onStateChange(SwitchStateChangeEvent event) {
 				if(event.getNewState() == SwitchState.ON) {
-					piFace.getLed(PiFaceLed.LED0).blink(0);
+					piFace.getLed(PiFaceLed.LED0).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED0).off();
-					piFace.getLed(PiFaceLed.LED1).blink(0);
+					piFace.getLed(PiFaceLed.LED1).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED1).off();
-					piFace.getLed(PiFaceLed.LED2).blink(0);
+					piFace.getLed(PiFaceLed.LED2).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED2).off();
-					piFace.getLed(PiFaceLed.LED3).blink(0);
+					piFace.getLed(PiFaceLed.LED3).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED3).off();
-					piFace.getLed(PiFaceLed.LED4).blink(0);
+					piFace.getLed(PiFaceLed.LED4).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED4).off();
-					piFace.getLed(PiFaceLed.LED5).blink(0);
+					piFace.getLed(PiFaceLed.LED5).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED5).off();
-					piFace.getLed(PiFaceLed.LED6).blink(0);
+					piFace.getLed(PiFaceLed.LED6).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED6).off();
-					piFace.getLed(PiFaceLed.LED7).blink(0);
+					piFace.getLed(PiFaceLed.LED7).blink(LED_OFF);
 					piFace.getLed(PiFaceLed.LED7).off();
 				}
 			}
 		});	
 	}
 	
-	
-	
-	
-	
 	// main method to run the code
 	public static void main(String args[]) throws InterruptedException, IOException, CommunicationException {
-		List<Sendable> emergencies;
+		// initialize variables used in main
 		Socket sock;
 		InetAddress ip;
 		
-		
 		EmergencySystem emergency = new EmergencySystem();
-		
-		
-		
 		Scanner in = new Scanner(System.in);
-		// Take in the location of the database / controller 
-		System.out.println("\n please enter the IP of the Data base 10.0.0.X "); 
+		// Take in the location of the database/controller 
+		System.out.println("\n Please enter the IP of the controller 10.0.0.X "); 
 		emergency.setHostIP("10.0.0."+ in.next());
-		System.out.println("\n The IP of the client is:" +emergency.getHostIP());
+		System.out.println("\n The IP of the controller is:" + emergency.getHostIP());
 	
-		//Take in the port of the client 
-		System.out.println("\n please enter the clients port number ");
+		// Take in the port of the client 
+		System.out.println("\n Please enter the database port number ");
 		emergency.setHostPort(in.nextInt());
-		System.out.println("The clients port number is: " + emergency.getHostPort());
-
-		System.out.println(emergency.getHostPort());
-		
-	
+		System.out.println("The database port number is: " + emergency.getHostPort());
 		
 		ip = InetAddress.getByName(emergency.getHostIP());
-		//use the connect to method to set the socket 
+		// Use the connect to method to set the socket 
 		emergency.host(emergency.getHostPort(),ip);
-		
 
-		System.out.println("about to set the sock");
 		sock = emergency.acceptClient();	
-		System.out.println("succesfully loaded socket");
-
+		System.out.println("succesfully connected");
+		
+		// creating an infinite loop to keep receiving signals
 		boolean waiting = true;
 		while(waiting){
-			
-				emergencies=emergency.receive(sock);
-
-				System.out.println("made it past recieve");
-
-				if(emergencies != null){
-					emergency.setUpButtonHandler();	
-				}
-			
+				//creates thread
+				(new Thread(new Runnable() {
+					public void run() {
+						List<Sendable> emergencies = null;
+						try {
+							emergencies=emergency.receive(sock);
+						} catch (CommunicationException e) {
+							e.printStackTrace();
+						}
+						if(emergencies != null){
+							emergency.setUpButtonHandler();	
+						}
+					}
+					
+				})).start();
 		}
-		
 		emergency.disconnectHost();
 		in.close();
 	}
-
-
 }
